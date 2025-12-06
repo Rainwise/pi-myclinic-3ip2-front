853d2d6a29ce9825f50d1910e4c5900f0ab94423
Ivor <picekivor@gmail.com>
2025-12-06 18:35:25 +0100

    Prepared patient dashboard with navlinks and header components

---
+++ src/app/dashboard/doctors/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import { Stack } from "@mantine/core";
+
+export default function DoctorsPage() {
+  return <Stack gap={"md"}>CRUD DOCTORS PAGE</Stack>;
+}
---
+++ src/components/dashboardSidebar/dashboardSidebarStyles.ts
@@ -0,0 +1,10 @@
+export const dashboardSidebarStyles = {
+  activeLink: "var(--mantine-color-primary-2)",
+  inactiveLinkHover: "var(--mantine-color-primary-1)",
+  inactiveLinkHoverAlt: "var(--mantine-color-bg-4)",
+  activeText: "var(--mantine-color-text-4)",
+  inactiveText: "var(--mantine-color-text-0)",
+  secondaryAction: "var(--mantine-color-secondary-1)",
+  secondaryActiveLink: "var(--mantine-color-secondary-2)",
+  secondaryInactiveLinkHover: "var(--mantine-color-secondary-2)",
+};
