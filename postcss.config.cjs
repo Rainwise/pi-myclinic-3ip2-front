34a1fc9cbe8d3242da4f2c986b90698ecedadbcc
Ivor <picekivor@gmail.com>
2025-12-06 17:29:26 +0100

    Prepared frontend stack with Next.JS and Mantine Component Library

---
+++ postcss.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: {
+    "postcss-preset-mantine": {},
+    "postcss-simple-vars": {
+      variables: {
+        "mantine-breakpoint-xs": "30em",
+        "mantine-breakpoint-sm": "48em",
+        "mantine-breakpoint-md": "56em",
+        "mantine-breakpoint-lg": "64em",
+        "mantine-breakpoint-xl": "90em",
+      },
+    },
+  },
+};
